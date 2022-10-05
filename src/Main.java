bcb0b3e102f3a278f51aee2f3989fe05218b1455
Tony <yaraliev5@gmail.com>
2022-10-05 14:09:36 +0300

    hw8

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,12 @@ import java.util.DoubleSummaryStatistics;
 
 public class Main {
     public static void main(String[] args) {
-        int[] weihts = {64, 65, 66, 67, 65, 68, 69, 0, 0, 0, 0, 0};
-        weihts[0] = 65;
-        int januaryWei = weihts[0];
-        System.out.println(januaryWei);
-        System.out.println(weihts[0]);
-        System.out.println(weihts[4]);
-        int janurt = 0;
-        System.out.println(weihts[janurt]);
-        for (int i = 0; i < weihts.length; i++) ;
 
 
-        int[] wei = new int[] {1, 2, 3};
+        int[] wei = new int[3];
+        wei[0]=1;
+        wei[1]=2;
+        wei[2]=3;
         System.out.print(wei[0] + "," + wei[1] + "," + wei[2]);
         System.out.println();
         System.out.print(wei[2] + "," + wei[1] + "," + wei[0]);
@@ -28,13 +22,11 @@ public class Main {
 
         int[] numb= new int [3];
         numb [0]=1;
-        for (int y=0;y<numb.length;y++) {
-            numb[y]=y+1;
-
-            if (numb[y]% 2 == 1) ;
+        int y = 0;
+        if (numb[y]% 2 == 1) {
             numb[y]=numb[y]+1;
-            System.out.println(numb[y]);
         }
+            System.out.println(numb[y]);
         
 
 
